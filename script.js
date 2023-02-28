e336a7d363e00e9a3f84f602a098df02ab9059fa
sjjames020 <sjjames020@gmail.com>
2023-02-28 14:54:55 -0500

    Worked on flip function

---
+++ script.js
@@ -0,0 +1,16 @@
+/////////////////////
+///Variables
+/////////////////////
+
+const memoryCards = document.querySelectorAll(".memory-card");
+
+/////////////////////
+///Functions
+/////////////////////
+function flipCard() {
+ this.classList.toggle('flip')
+}
+
+memoryCards.forEach((memoryCard) =>
+  memoryCard.addEventListener("click", flipCard)
+);
