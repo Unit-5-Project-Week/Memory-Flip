2c56934c57aa70743924771ba6e16d1907929607
cliffentei <cliffeti776@gmail.com>
2023-02-28 17:16:56 -0500

    Implemented Matching

--- script.js
+++ script.js
@@ -3,12 +3,28 @@
 /////////////////////
 
 const memoryCards = document.querySelectorAll(".memory-card");
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 /////////////////////
 ///Functions
 /////////////////////
 function flipCard() {
-  this.classList.toggle("flip");
+  this.classList.add("flip");
+
+  if (!hasFlippedCard){
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    // second click 
+    hasFlippedCard = false;
+    secondCard = this;
+
+    // do cards match?
+    console.log(firstCard.dataset.image);
+    console.log(secondCard.dataset.image);
+  }
 }
 
 memoryCards.forEach((memoryCard) =>
