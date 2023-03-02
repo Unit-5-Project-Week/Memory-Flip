d93255b60b4edec3392de9bb49b313dd1fbcbfe4
cliffentei <cliffeti776@gmail.com>
2023-03-01 22:26:02 -0500

    Testing testing branch

--- start.js
+++
@@ -1,9 +0,0 @@
-const avatar = document.querySelector(".avatar");
-
-function startRedirect() {
-  setTimeout((e) => {
-    location.href = "start.html";
-  }, 4500);
-}
-
-avatar.addEventListener("click", startRedirect);
